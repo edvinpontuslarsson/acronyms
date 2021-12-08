93b8915833d142824e082e8c31e7b32bb2c71f19
Edvin Larsson <edvinpontuslarsson@gmail.com>
2021-12-08 16:57:22 +0100

    initial text area

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    // console.log(text);
+  }, [text]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+        cols={60}
+        rows={30}
+      ></textarea>
     </div>
   );
 }
-
-export default App;
