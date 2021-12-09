d195bcaabab328a0f4d191d48a8fc66cd2610aef
Edvin Larsson <edvinpontuslarsson@gmail.com>
2021-12-09 10:15:51 +0100

    acronymCollection

---
+++ src/data/provider.ts
@@ -0,0 +1,13 @@
+interface IAcronymCollection {
+  [key: string]: string;
+}
+
+export const acronymCollection: IAcronymCollection = {
+  gh: 'GitHub',
+  ts: 'TypeScript',
+  lorem: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et dictum eros. Etiam fringilla ultrices elit ac finibus. Sed risus felis, venenatis at velit nec, tempor tincidunt arcu. Nunc sit amet elit luctus, feugiat nibh id, tincidunt libero. Nunc aliquam pulvinar ex egestas volutpat. Mauris quis velit urna. Quisque fermentum quam molestie odio posuere tempus. Nunc vulputate tellus augue, sit amet ultricies ex euismod eget. Maecenas mattis dictum dui et ornare. Ut id elit a metus aliquet gravida. Donec laoreet mattis dapibus. Fusce vulputate feugiat sapien ut rutrum. Etiam at arcu placerat, iaculis tellus vitae, lacinia leo. Vestibulum mollis sollicitudin feugiat. Morbi dictum non nisi nec congue.
+
+  Curabitur gravida risus augue, eget maximus sapien sagittis sit amet. Cras leo tortor, efficitur ut diam a, fermentum tempor elit. Phasellus ut pharetra est. Suspendisse potenti. Nullam hendrerit dapibus metus, eu consequat orci auctor at. Morbi vestibulum dignissim massa, a condimentum leo. Curabitur ut odio placerat, facilisis magna eu, tempor justo. Donec tincidunt nibh quam, sed scelerisque orci aliquet et. Quisque eu nunc ut nibh fermentum scelerisque. Sed placerat ligula sit amet est posuere, at auctor ex mollis. Phasellus orci nulla, luctus sed molestie at, ullamcorper ut turpis. In semper felis et purus ornare, eget efficitur dolor euismod. Morbi ultrices, urna ac dictum condimentum, ligula magna consectetur ligula, et aliquet turpis tellus at turpis. Sed convallis leo elit, in pellentesque lectus dapibus eget. Cras aliquet posuere libero, dapibus accumsan neque.
+  
+  Nam feugiat quam ante, vel condimentum elit imperdiet sit amet. Proin tincidunt lectus a lorem faucibus placerat. Vivamus in nisl in tellus fermentum rutrum. Morbi vitae faucibus sapien, egestas mollis augue. Suspendisse augue tellus, consectetur mollis mollis mollis, congue non tortor. Cras blandit pellentesque metus, vel lacinia ex lobortis et. Vivamus tincidunt felis non suscipit aliquam. Duis non eros lacus. Nulla consectetur mattis est vel volutpat.`,
+};
